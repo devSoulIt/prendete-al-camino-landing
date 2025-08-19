0c81037927989bfdc68c3c7cf86b6f96fe2ef029
Lautaro Romano <71612453+LautaroRomano@users.noreply.github.com>
2025-08-19 18:41:53 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,14 @@ import { useEffect, useState } from "react";
 
 export function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
 
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY;
       const windowHeight = window.innerHeight;
-      
+
       // Cambiar el background cuando el scroll pase el tamaño de la pantalla
       if (scrollPosition > windowHeight) {
         setIsScrolled(true);
@@ -21,7 +23,7 @@ export function Header() {
     };
 
     window.addEventListener('scroll', handleScroll);
-    
+
     // Limpiar el event listener cuando el componente se desmonte
     return () => {
       window.removeEventListener('scroll', handleScroll);
@@ -29,11 +31,66 @@ export function Header() {
   }, []);
 
   return (
-    <header className={`sticky top-0 z-50 w-full min-h-[90px] transition-colors duration-700 ${
-      isScrolled 
-        ? 'bg-black/70 backdrop-blur-sm shadow-lg' 
+    <header className={`sticky top-0 z-50 w-full min-h-[90px] transition-colors duration-700 ${isScrolled
+        ? 'bg-black/70 backdrop-blur-sm shadow-lg'
         : 'bg-transparent'
-    }`}>
+      }`}>
+      {isMenuOpen && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black/70 backdrop-blur-sm z-50 flex justify-center items-start" onClick={() => setIsMenuOpen(false)}>
+          <div className="max-w-7xl mx-auto px-4">
+            <div className="flex justify-between items-center py-1">
+              <nav className="flex flex-col space-y-4 text-center text-2xl justify-center items-center mt-20">
+                <div className="flex items-center mb-10" >
+                  <Image src="/images/logo.png" alt="Logo" width={80} height={80} />
+                </div>
+                <a
+                  href="#inicio"
+                  className="transition-colors duration-200 text-white hover:text-primary font-bold uppercase"
+                >
+                  Inicio
+                </a>
+                <a
+                  href="#santiago"
+                  className="transition-colors duration-200 text-white hover:text-primary font-bold uppercase"
+                >
+                  Santiago de Compostela
+                </a>
+                <a
+                  href="#viajes-cortos"
+                  className="transition-colors duration-200 text-white hover:text-primary font-bold uppercase"
+                >
+                  Viajes Cortos
+                </a>
+                <a
+                  href="#videollamada"
+                  className="transition-colors duration-200 text-white hover:text-primary font-bold uppercase"
+                >
+                  Videollamada
+                </a>
+                <a
+                  href="#galeria"
+                  className="transition-colors duration-200 text-white hover:text-primary font-bold uppercase"
+                >
+                  Galería
+                </a>
+                <a
+                  href="#contacto"
+                  className="transition-colors duration-200 text-white hover:text-primary font-bold uppercase"
+                >
+                  Contacto
+                </a>
+              </nav>
+              <Button
+                className="hidden md:inline-flex text-white font-medium px-6 py-2 rounded-lg hover:opacity-90 transition-opacity duration-200"
+                style={{ backgroundColor: '#ffd600', color: '#404d21' }}
+              >
+                Solicitar información
+              </Button>
+            </div>
+          </div>
+        </div>
+      )}
+
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between items-center py-1">
           {/* Logo */}
@@ -90,7 +147,7 @@ export function Header() {
           </Button>
 
           {/* Mobile menu button */}
-          <button className="md:hidden p-2 rounded-md text-white hover:text-gray-300 hover:bg-white/10 transition-colors duration-200">
+          <button className="md:hidden p-2 rounded-md text-white hover:text-gray-300 hover:bg-white/10 transition-colors duration-200" onClick={() => setIsMenuOpen(!isMenuOpen)}>
             <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
             </svg>
